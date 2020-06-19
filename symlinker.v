79f0f619705cd204b6625e6c8ff5f4ab51428616
Lukas Neubert <lukas.neubert3@gmail.com>
2020-06-19 10:11:16 +0200

    add help command

--- symlinker.v
+++ symlinker.v
@@ -2,16 +2,32 @@ module main
 
 import os
 
+const (
+	help_text =
+'symlinker
+
+Usage: symlinker [command]
+
+Commands:
+  help    Show this message.'
+)
+
+fn show_help() {
+	println(help_text)
+}
+
 fn main() {
 	args := os.args[1..]
 
 	if args.len == 0 {
+		show_help()
 		return
 	}
 
 	match args[0] {
+		'help' { show_help() }
 		else {
-			println('${args[0]}: unknown command')
+			println('${args[0]}: unknown command\nRun "symlinker help" for usage.')
 		}
 	}
 }
