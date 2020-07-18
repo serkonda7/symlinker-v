8c5dc2b6e2c1d414c7b34528cfc9a163201d0bf1
Lukas Neubert <lukas.neubert3@gmail.com>
2020-07-18 18:02:58 +0200

    simplify scope managment
    - list: fix invalid scope with --all
    - remove struct commas

--- symlinker.v
+++ symlinker.v
@@ -5,12 +5,15 @@ import os
 import etienne_napoleone.chalk
 
 const (
-	local_link_dir  = os.home_dir() + '.local/bin/'
-	global_link_dir = '/usr/local/bin/'
+	scope_dirs = {
+		'local': os.home_dir() + '.local/bin/'
+		'global': '/usr/local/bin/'
+	}
 )
 
 fn add_link(cmd cli.Command) {
-	link_dir := actual_link_dir(cmd)
+	scope := get_scope(cmd)
+	link_dir := scope_dirs[scope]
 	if !os.exists(link_dir) {
 		os.mkdir_all(link_dir)
 	}
@@ -28,7 +31,7 @@ fn add_link(cmd cli.Command) {
 	link_path := link_dir + link_name
 	if os.exists(link_path) {
 		if os.is_link(link_path) {
-			err_and_exit('${scope(cmd)} link named "$link_name" already exists', '')
+			err_and_exit('$scope link with name "$link_name" already exists', '')
 		}
 		err_and_exit('File named "$link_name" already exists', '')
 	}
@@ -36,24 +39,26 @@ fn add_link(cmd cli.Command) {
 	os.symlink(file_path, link_path) or {
 		err_and_exit('Permission denied', 'Run with "sudo" instead.')
 	}
-	println('Created ${scope(cmd)} link: "$link_name"')
+	println('Created $scope link: "$link_name"')
 }
 
 fn delete_link(cmd cli.Command) {
+	scope := get_scope(cmd)
+	link_dir := scope_dirs[scope]
 	for arg in cmd.args {
-		link_path := actual_link_dir(cmd) + arg
+		link_path := link_dir + arg
 
 		if !os.is_link(link_path) {
 			if !os.exists(link_path) {
-				print_err('${scope(cmd)} link "$arg" does not exist', '')
+				print_err('$scope link "$arg" does not exist', '')
 			}
-			print_err('"$arg" is no ${scope(cmd)} link', '')
+			print_err('"$arg" is no $scope link', '')
 		}
 
 		os.rm(link_path) or {
 			err_and_exit('Permission denied', 'Run with "sudo" instead.')
 		}
-		println('Deleted ${scope(cmd)} link: "$arg"')
+		println('Deleted $scope link: "$arg"')
 	}
 }
 
@@ -61,9 +66,9 @@ fn list_links(cmd cli.Command) {
 	f_all := cmd.flags.get_bool('all') or { panic(err) }
 
 	dirs := if f_all {
-		[local_link_dir, global_link_dir]
+		[scope_dirs['local'], scope_dirs['global']]
 	} else {
-		[actual_link_dir(cmd)]
+		[scope_dirs[get_scope(cmd)]]
 	}
 
 	for i, dir in dirs {
@@ -71,11 +76,11 @@ fn list_links(cmd cli.Command) {
 		links := files.filter(os.is_link(dir + it))
 
 		if links.len == 0 {
-			println('No ${scope(cmd)} symlinks detected.')
+			println('No ${get_scope_by_dir(dir)} symlinks detected.')
 			return
 		}
 
-		println(chalk.style('${scope_by_dir(dir)} links:', 'bold'))
+		println(chalk.style('${get_scope_by_dir(dir)} links:', 'bold'))
 		f_real := cmd.flags.get_bool('real') or { panic(err) }
 		if f_real {
 			for link in links {
@@ -93,23 +98,18 @@ fn list_links(cmd cli.Command) {
 }
 
 fn open_link_folder(cmd cli.Command) {
-	link_dir := actual_link_dir(cmd)
+	link_dir := scope_dirs[get_scope(cmd)]
 	command := 'xdg-open $link_dir'
 	os.exec(command) or { panic(err) }
 }
 
-fn actual_link_dir(cmd cli.Command) string {
-	is_global := cmd.flags.get_bool('global') or { panic(err) }
-	return if is_global { global_link_dir } else { local_link_dir }
-}
-
-fn scope(cmd cli.Command) string {
+fn get_scope(cmd cli.Command) string {
 	is_global := cmd.flags.get_bool('global') or { panic(err) }
 	return if is_global { 'global' } else { 'local' }
 }
 
-fn scope_by_dir(dir string) string {
-	return if dir == local_link_dir { 'local' } else { 'global' }
+fn get_scope_by_dir(dir string) string {
+	return if dir == scope_dirs['local'] { 'local' } else { 'global' }
 }
 
 fn print_err(msg, tip_msg string) {
@@ -126,47 +126,47 @@ fn err_and_exit(msg, tip_msg string) {
 
 fn main() {
 	mut cmd := cli.Command{
-		name: 'symlinker',
-		version: '0.6.0',
+		name: 'symlinker'
+		version: '0.6.0'
 		disable_flags: true
 		sort_commands: false
 	}
 	cmd.add_flag(cli.Flag{
-		flag: .bool,
-		name: 'global',
-		abbrev: 'g',
+		flag: .bool
+		name: 'global'
+		abbrev: 'g'
 		description: 'Execute the command machine-wide.'
 		global: true
 	})
 
 	mut add_cmd := cli.Command{
-		name: 'add',
-		description: 'Create a symlink to <file>.',
+		name: 'add'
+		description: 'Create a symlink to <file>.'
 		execute: add_link
 	}
 	add_cmd.add_flag(cli.Flag{
-		flag: .string,
-		name: 'name',
-		abbrev: 'n',
+		flag: .string
+		name: 'name'
+		abbrev: 'n'
 		description: 'Use a custom name for the link.'
 	})
 
 	mut del_cmd := cli.Command{
-		name: 'del',
-		description: 'Delete all specified symlinks.',
+		name: 'del'
+		description: 'Delete all specified symlinks.'
 		execute: delete_link
 	}
 
 	mut list_cmd := cli.Command{
-		name: 'list',
-		description: 'List all symlinks.',
+		name: 'list'
+		description: 'List all symlinks.'
 		execute: list_links
 	}
 	list_cmd.add_flags([
 		cli.Flag {
-			flag: .bool,
-			name: 'real',
-			abbrev: 'r',
+			flag: .bool
+			name: 'real'
+			abbrev: 'r'
 			description: 'Also print the path the links point to.'
 		},
 		cli.Flag {
@@ -178,8 +178,8 @@ fn main() {
 	])
 
 	mut open_cmd := cli.Command{
-		name: 'open',
-		description: 'Open symlink folder in the file explorer.',
+		name: 'open'
+		description: 'Open symlink folder in the file explorer.'
 		execute: open_link_folder
 	}
 
