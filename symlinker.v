ec8bb51b9b4fe28caab5fd4981e20c9e64290d69
Lukas Neubert <lukas.neubert3@gmail.com>
2020-08-21 13:41:56 +0200

    run fmt; get flags with or values

--- symlinker.v
+++ symlinker.v
@@ -17,17 +17,14 @@ fn create_link(cmd Command) {
 	if !os.exists(link_dir) {
 		os.mkdir_all(link_dir)
 	}
-
 	file_path := os.real_path(cmd.args[0])
 	if !os.exists(file_path) {
 		err_and_exit('Cannot link inexistent file "$file_path"', '')
 	}
-
-	mut link_name := cmd.flags.get_string('name') or { panic(err) }
+	mut link_name := cmd.flags.get_string_or('name', '')
 	if link_name == '' {
 		link_name = cmd.args[0].split('/').last()
 	}
-
 	link_path := link_dir + link_name
 	if os.exists(link_path) {
 		if os.is_link(link_path) {
@@ -35,7 +32,6 @@ fn create_link(cmd Command) {
 		}
 		err_and_exit('File named "$link_name" already exists', '')
 	}
-
 	os.symlink(file_path, link_path) or {
 		err_and_exit('Permission denied', 'Run with `sudo` instead.')
 	}
@@ -48,7 +44,6 @@ fn delete_link(cmd Command) {
 	mut err := 0
 	for arg in cmd.args {
 		link_path := link_dir + arg
-
 		if !os.is_link(link_path) {
 			if !os.exists(link_path) {
 				print_err('$scope link `$arg` does not exist', 'Run `symlinker list` to see your links.')
@@ -72,14 +67,16 @@ fn delete_link(cmd Command) {
 fn list_links(cmd Command) {
 	for _, dir in link_dirs {
 		scope := get_scope_by_dir(dir)
-		files := os.ls(dir) or { panic(err) }
+		files := os.ls(dir) or {
+			panic(err)
+		}
 		links := files.filter(os.is_link(dir + it))
 		if links.len == 0 {
 			println('No $scope symlinks detected.')
 			continue
 		}
 		println(chalk.style('$scope links:', 'bold'))
-		f_real := cmd.flags.get_bool('real') or { panic(err) }
+		f_real := cmd.flags.get_bool_or('real', false)
 		if f_real {
 			mut invalid_links := []string{}
 			for link in links {
@@ -94,8 +91,7 @@ fn list_links(cmd Command) {
 			for inv_link in invalid_links {
 				println(chalk.fg('  INVALID', 'light_magenta') + ' $inv_link')
 			}
-		}
-		else {
+		} else {
 			mut rows := []string{}
 			mut row_idx := 0
 			for i, link in links {
@@ -130,13 +126,17 @@ fn update_link(cmd Command) {
 		err_and_exit('Cannot update inexistent link "$link_path"', '')
 	}
 	if new_name != '' {
-		os.mv_by_cp(link_path, dir + new_name) or { panic(err) }
+		os.mv_by_cp(link_path, dir + new_name) or {
+			panic(err)
+		}
 		link_path = dir + new_name
 		link_name = new_name
 		println('Renamed $scope link "$link_name" to ${new_name}.')
 	}
 	if new_path != '' {
-		os.rm(link_path) or { panic(err) }
+		os.rm(link_path) or {
+			panic(err)
+		}
 		os.symlink(new_path, link_path) or {
 			err_and_exit('Permission denied', 'Run with "sudo" instead.')
 		}
@@ -152,7 +152,9 @@ fn open_link_folder(cmd Command) {
 	mut dir := get_dir(scope)
 	if cmd.args.len >= 1 {
 		target_link := cmd.args[0]
-		links := os.ls(dir) or { panic(err) }
+		links := os.ls(dir) or {
+			panic(err)
+		}
 		if target_link in links {
 			dir = os.real_path(dir + target_link).all_before_last('/')
 			println('Opening the directory of "$target_link"...')
@@ -163,17 +165,27 @@ fn open_link_folder(cmd Command) {
 		println('Opening the $scope symlink folder...')
 	}
 	command := 'xdg-open $dir'
-	os.exec(command) or { panic(err) }
+	os.exec(command) or {
+		panic(err)
+	}
 }
 
 fn get_scope(cmd Command) string {
-	machine_wide := cmd.flags.get_bool('machine') or { panic(err) }
-	return if machine_wide { 'machine-wide' } else { 'user' }
+	machine_wide := cmd.flags.get_bool_or('machine', false)
+	return if machine_wide {
+		'machine-wide'
+	} else {
+		'user'
+	}
 }
 
 fn get_scope_by_dir(dir string) string {
 	$if linux {
-		return if dir == link_dirs['user'] { 'user' } else { 'machine-wide' }
+		return if dir == link_dirs['user'] {
+			'user'
+		} else {
+			'machine-wide'
+		}
 	} $else {
 		panic('Invalid os')
 	}
@@ -213,7 +225,6 @@ fn main() {
 		description: 'Execute the command machine-wide.'
 		global: true
 	})
-
 	mut link_cmd := Command{
 		name: 'link'
 		description: 'Create a symlink to <file>.'
@@ -226,28 +237,23 @@ fn main() {
 		abbrev: 'n'
 		description: 'Use a custom name for the link.'
 	})
-
 	mut del_cmd := Command{
 		name: 'del'
 		description: 'Delete all specified symlinks.'
 		required_args: 1
 		execute: delete_link
 	}
-
 	mut list_cmd := Command{
 		name: 'list'
 		description: 'List all symlinks.'
 		execute: list_links
 	}
-	list_cmd.add_flag(
-		Flag {
-			flag: .bool
-			name: 'real'
-			abbrev: 'r'
-			description: 'Also print the path the links point to.'
-		}
-	)
-
+	list_cmd.add_flag(Flag{
+		flag: .bool
+		name: 'real'
+		abbrev: 'r'
+		description: 'Also print the path the links point to.'
+	})
 	mut update_cmd := Command{
 		name: 'update'
 		description: 'Rename symlinks or update their real path.'
@@ -265,15 +271,13 @@ fn main() {
 			name: 'path'
 			abbrev: 'p'
 			description: 'The new path.'
-		}
+		},
 	])
-
 	mut open_cmd := Command{
 		name: 'open'
 		description: 'Open symlink folder in the file explorer.'
 		execute: open_link_folder
 	}
-
 	cmd.add_commands([link_cmd, del_cmd, list_cmd, /* update_cmd, */ open_cmd])
 	cmd.parse(os.args)
 }
