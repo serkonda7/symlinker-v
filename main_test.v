b52a87a37cce66d3fa4828e39d6c2221f580e6dc
Lukas Neubert <serkonda@protonmail.com>
2021-03-24 21:58:20 +0100

    cleanup main_test.v

--- main_test.v
+++ main_test.v
@@ -1,29 +1,5 @@
 module main
 
-import os
-
-const (
-	troot   = os.temp_dir() + '/symlinker'
-	tsource = troot + '/tfiles/'
-	link1   = 'link1'
-	link2   = 'link2'
-	link3   = 'link3'
-)
-
-fn testsuite_begin() {
-	os.rmdir_all(troot) or {}
-	os.mkdir_all(tsource) or { panic(err) }
-	os.chdir(tsource)
-	os.write_file(link1, '') or { panic(err) }
-	os.write_file(link2, '') or { panic(err) }
-	os.write_file(link3, '') or { panic(err) }
-}
-
-fn testsuite_end() {
-	os.chdir(os.wd_at_startup)
-	os.rmdir_all(troot) or {}
-}
-
 fn test_new_app() {
 	app := new_app()
 	assert app.commands.len == 5
@@ -61,40 +37,3 @@ fn test_name_max() {
 	expected := 5
 	assert name_max(names) == expected
 }
-
-/*
-fn test_link_cmd() {
-	mut app := new_app()
-	app.parse(['', 'link', 'link1'])
-	app = new_app()
-	app.parse(['', 'link', '--name', 'alt_link', 'link2'])
-	app = new_app()
-	app.parse(['', 'link', 'link3'])
-	app = new_app()
-	app.parse(['', 'link', '--machine', 'link3'])
-}
-
-fn test_list_cmd() {
-	mut app := new_app()
-	app.parse(['', 'list'])
-	app = new_app()
-	app.parse(['', 'list', '--real'])
-}
-
-fn test_update_cmd() {
-	mut app := new_app()
-	app.parse(['', 'update', '--name', 'alt2', 'alt_link'])
-	app = new_app()
-	app.parse(['', 'update', '--path', 'link1', 'alt2'])
-	app = new_app()
-	app.parse(['', 'update', '--name', 'alt_link', '--path', 'link2', 'alt2'])
-}
-
-fn test_del_cmd() {
-	mut app := new_app()
-	app.parse(['', 'del', 'link1'])
-	app = new_app()
-	app.parse(['', 'del', 'alt_link', 'link3'])
-}
-*/
-// TODO: open
